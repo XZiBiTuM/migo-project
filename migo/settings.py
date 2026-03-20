ace4b279ce2663df27f5216ebb07d1f3221dedf6
XZiBiTuM <balayi323161@mail.ru>
2026-03-20 12:49:41 +0300

    Final fix for static files and Nginx config

--- migo/settings.py
+++ migo/settings.py
@@ -127,13 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    MEDIA_ROOT,
+    # Путей здесь пока нет (базовые файлы в приложениях),
+    # или можно добавить другие папки
 ]
 
 REST_FRAMEWORK = {
