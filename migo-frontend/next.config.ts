c018058df3969348b99554a31126b20ec8009dd1
XZiBiTuM <balayi323161@mail.ru>
2026-03-24 12:53:06 +0300

    Fixed SEO issues v2.4

--- migo-frontend/next.config.ts
+++ migo-frontend/next.config.ts
@@ -4,6 +4,28 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
